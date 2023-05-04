b08e6f41a7c7c53f96d2cd6bf5155ac080fc8280
Kenji <70079754+OkayKenji@users.noreply.github.com>
2023-05-03 20:48:51 -0400

    TeamPython dependency

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,27 @@
-import java.util.ArrayList;
-
-public class Main {
+// Main class was created in collaboration with Team Python, Team Java, and Team Prolog-Other
+class Main {
     public static void main(String[] args) {
-        SightingReader aaa = new SightingReader();
-        ArrayList<Sighting> bbb =  aaa.getSightings("sightings.csv");
+        // Creation of the animal monitor class (from TeamPython)
+        AnimalMonitor animalMonitor = new AnimalMonitor();
+
+        // loads the .csv file, uses the SightingsReader (from TeamJava) and an ArrayList of Sightings (from TeamProlog-Other)
+        animalMonitor.addSightings("src/main/java/sightings.csv");
+
+        // All sightings
+        System.out.println("Printing out all sightings:");
+        animalMonitor.printList();
+
+        // Mountain Gorilla stats
+        System.out.println("------------------------------------");
+        System.out.println("There are " + animalMonitor.getCount("Mountain Gorilla") + " Mountain Gorillas" );
+        System.out.println("Here are the specific sightings of the Mountain Gorillas:");
+        animalMonitor.printSightingsOf("Mountain Gorilla");
+
+        // Spotter 1 stats
+        System.out.println("------------------------------------");
+        System.out.println("Here are the specific sightings by spotter 1:");
+        animalMonitor.printSightingsBy(1);
+
+
     }
 }
